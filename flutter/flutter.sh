274394859ce4c16f8bc26219797ecbc27afd75ea
ninofelino12 <53456354+ninofelino12@users.noreply.github.com>
2022-12-01 08:23:00 +0700

    Update flutter.sh

--- flutter/flutter.sh
+++ flutter/flutter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Instal flutter"
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.3.9-stable.tar.xz
+#wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.3.9-stable.tar.xz
 tar xf flutter_linux_3.3.9-stable.tar.xz
 export PATH="$PATH:`pwd`/flutter/bin"
 flutter precache
