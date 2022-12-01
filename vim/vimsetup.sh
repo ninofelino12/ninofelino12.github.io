1e6ca40506263c3fbf7c01365f5798081b97f6a1
ninofelino12 <53456354+ninofelino12@users.noreply.github.com>
2022-12-01 21:46:33 +0700

    Update vimsetup.sh

--- vim/vimsetup.sh
+++ vim/vimsetup.sh
@@ -1,2 +1,4 @@
+sudo mkdir  ~/.config/nvim/
+sudo nvim ~/.config/nvim/init.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
