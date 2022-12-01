735ca01127fc23caac2219e03a6cfcf1d6a7f9d7
ninofelino12 <53456354+ninofelino12@users.noreply.github.com>
2022-12-01 08:17:50 +0700

    Update flutter.sh

--- flutter/flutter.sh
+++ flutter/flutter.sh
@@ -2,3 +2,10 @@
 echo "Instal flutter"
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.3.9-stable.tar.xz
 tar xf flutter_linux_3.3.9-stable.tar.xz
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter precache
+flutter doctor
+export PATH="$PATH:[PATH_OF_FLUTTER_GIT_DIRECTORY]/bin"
+echo $PATH
+which flutter
+sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
