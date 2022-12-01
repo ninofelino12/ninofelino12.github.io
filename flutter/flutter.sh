25b711ce2a8662b5c9d433c613fe09911dc1c0af
ninofelino12 <53456354+ninofelino12@users.noreply.github.com>
2022-12-01 08:05:36 +0700

    Create flutter.sh

---
+++ flutter/flutter.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Instal flutter"
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.3.9-stable.tar.xz
+tar xf flutter_linux_3.3.9-stable.tar.xz
