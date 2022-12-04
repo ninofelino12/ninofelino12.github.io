2d826b6e56048b4f799504c69d289c9556860d70
ninofelino12 <53456354+ninofelino12@users.noreply.github.com>
2022-12-04 15:52:27 +0700

    Update odoo-14.install.sh

--- termux/odoo-14.install.sh
+++ termux/odoo-14.install.sh
@@ -2,4 +2,6 @@ wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tar.xz
 tar xfv Python-3.10.8.tar.xz       
 git clone https://www.github.com/odoo/odoo --depth 1 --branch 14.0 --single-branch
 export PATH=($PWD):$PATH
+cd odoo
 pip -r ./odoo/requirements.txt
+
