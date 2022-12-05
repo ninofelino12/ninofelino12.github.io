a494d842e5c8cf32e4f0307be2e99089f88efc03
ninofelino12 <53456354+ninofelino12@users.noreply.github.com>
2022-12-05 17:36:52 +0700

    Update odoo-14.install.sh

--- termux/odoo-14.install.sh
+++ termux/odoo-14.install.sh
@@ -7,11 +7,13 @@ wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tar.xz
 tar xfv Python-3.10.8.tar.xz       
 git clone https://www.github.com/odoo/odoo --depth 1 --branch 14.0 --single-branch
 export PATH="Python-3.10.8/bin":$PATH
+export PATH="odoo/bin":$PATH
+sudo apt-get install pip
 cd odoo
 pip install -r ./odoo/requirements.txt
 sudo apt install python3-dev libpq-dev
 pip3 install psycopg2
 sudo apt-get install git build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libjpeg-dev gdebi -y
-
+echo $PATH"odoo-bin" >> start.sh
 
 
